38684f7f798a59fe6b531b0ddcf3951099056325
Matheus Moura <matheusmouraa@icloud.com>
2022-05-02 14:47:01 -0300

    envio de dados no script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // VARIÁVEIS
 
-var texto = document.querySelector("#texto");
+var texto = document.querySelector("#texto"); // TextArea
 var cifraDeCesarIn = document.querySelector("#cifraDeCesar");
 var base64 = document.querySelector("#base64");
-var btnEnviar = document.querySelector("#btn-enviar");
+var btnEnviar = document.querySelector("#btn-enviar"); // Btn Submit
 var incremento = document.querySelector("#incremento");
 var opcoes = document.querySelector("#opcoes");
 var radioCodificar = document.querySelector("#option-1");
 var radioDecodificar = document.querySelector("#option-2");
 var eCodificar = document.querySelector("#ecodificar");
 var incinput = document.querySelector("#incinput");
+var radioInputs = document.querySelector("#radioinputs");
+
 // FUNÇÃO ABRIR INCREMENTO
 
 opcoes.addEventListener("change", function () {
@@ -20,7 +22,7 @@ opcoes.addEventListener("change", function () {
   }
 });
 
-// FUNÇÃO MUDAR CODIFICAR PARA DECODIFICAR
+// FUNÇÃO MUDAR BTN CODIFICAR PARA DECODIFICAR
 
 radioCodificar.addEventListener("click", function () {
   if (true) {
@@ -36,14 +38,35 @@ radioDecodificar.addEventListener("click", function () {
 
 // FUNÇÃO FUTURA
 
-btnEnviar.addEventListener("click", function (e) {
+/* btnEnviar.addEventListener("click", function (e) {
   if (
     texto.value.length > 0 &&
     opcoes.value == "cifraDeCesar" &&
-    incinput.value < 99
+    incinput.value < 99 &&
+    radioCodificar.value == "codificar"
   ) {
     alert(+texto.value.length + +incinput.value);
   } else {
     alert("Há algo de errado");
   }
+}); */
+
+// FUNÇÃO CODIFICAR TEXTO
+
+var resultadoCode = btnEnviar.addEventListener("click", function (e) {
+  e.preventDefault();
+  var resultado = "";
+  for (var i = 0; i < texto.value.length; i++) {
+    var asciiNum = texto.value[i].charCodeAt();
+    if (asciiNum >= 65 && asciiNum <= 77) {
+      resultado += String.fromCharCode(asciiNum + +incinput.value);
+    } else if (asciiNum >= 78 && asciiNum <= 90) {
+      resultado += String.fromCharCode(asciiNum - +incinput.value);
+    }
+  }
+  console.log(resultado);
 });
+// [a, b, c, d, e, f, g, h, i, j, k, 1, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+// Change the inputs below to test
+
+// rot13("y")
